2ce86af20bdab05782f3eaf1554099e568650a37
Tyler Flint <tylerflint@gmail.com>
2017-09-27 09:23:32 -0600

    Add comments and structure to ubuntu bootstrap for clarity

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
-# this is a shell script that will be sourced, and should setup the boot process for a centos platform
 
-get_menu_vars() {
+# in order to generate the iPXE menu and grub conf, we need to set some
+# global variables to be used throughout this script
+set_menu_vars() {
+  # when iPXE boots, it will need to have a minimal network configuration,
+  # so we need to extract the external interface/ip from the host to set
+  # in the iPXE menu configuration.
   read -r menu_gateway nic <<<$(netstat -nr | awk '/^0.0.0.0|^default/ {print $2" "$8}')
   interface=${nic:${#nic} - 1}
   menu_ip=$(ifconfig $nic | awk '/inet addr/ {print $2}' | cut -f2 -d':')
   menu_netmask=$(ifconfig $nic | awk '/inet addr/ {print $4}' | cut -f2 -d':')
+  
+  # fetch the uuid to inform grub of the root partition
   uuid=$(blkid -s UUID -o value $(df /boot | tail -n1 | awk '{print $1}'))
+  
+  # determine the boot path for grub
   boot_path=$(if mountpoint /boot > /dev/null; then echo ""; else echo "/boot"; fi)
 }
 
+# generate an iPXE boot script
 script_ipxe() {
   cat <<EOF
 #!ipxe
@@ -20,25 +29,24 @@ set net${interface}/gateway ${menu_gateway}
 set dns 8.8.8.8 
 ifopen net${interface} 
 kernel http://s3.amazonaws.com/tools.nanobox.io/bootstrap/memdisk 
-initrd http://s3.amazonaws.com/tools.nanobox.io/lvmify/v1/lvmify.iso 
+initrd http://s3.amazonaws.com/tools.nanobox.io/lvmify/v1/lvmify.iso
 imgargs memdisk iso raw 
 boot
 EOF
 }
 
-# Create ipxe dataset
+# download iPXE and configure it for the next boot
 setup_ipxe() {
   curl -k -s -o /boot/ipxe.krn -O http://s3.amazonaws.com/smartos.pagodagrid.io/live/boot/ipxe.krn
   echo "$(script_ipxe)" > /boot/script.ipxe
 }
 
+# generate a grub menu entry for ipxe
 09_ipxe() {
   cat <<EOF
 #!/bin/sh
 exec tail -n +3 \$0
-# This file provides an easy way to add custom menu entries.  Simply type the
-# menu entries you want to add after this comment.  Be careful not to change
-# the 'exec tail' line above.
+
 menuentry 'ipxe' {
         recordfail
         load_video
@@ -58,23 +66,32 @@ menuentry 'ipxe' {
 EOF
 }
 
-create_menu() {
+# create the ipxe menu entry for grub
+create_ipxe_grub_menu() {
+  # create a backup, just in case ;)
   if [[ ! -f /boot/grub/grub.bak ]]; then
     cp /boot/grub/grub.cfg /boot/grub/grub.bak
   fi
+  
+  # add the ipxe menu entry
   echo "$(09_ipxe)" > /etc/grub.d/09_ipxe
   chmod 755 /etc/grub.d/09_ipxe
+  
+  # generate the final grub config
   grub-mkconfig > /boot/grub/grub.cfg
+  
+  # remove the temporary ipxe menu entry
   rm /etc/grub.d/09_ipxe
 }
 
-echo "Getting OS vars..."
-get_menu_vars
+echo "Setting boot menu vars..."
+set_menu_vars
 
 echo "Configuring iPXE..."
 setup_ipxe
 
 echo "Configuring menu..."
-create_menu
+create_ipxe_grub_menu
 
+echo "Rebooting..."
 reboot
